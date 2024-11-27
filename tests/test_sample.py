b0e51eaaa06b8b1b4308723d67a327289cb85621
admin <terstation.com@gmail.com>
2024-11-27 22:08:19 +0200

    Added more test cases

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,3 +6,7 @@ def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
+def test_add_negative_numbers():
+    from main import add_numbers
+    assert add_numbers(-5, -3) == -8
+    assert add_numbers(10, -10) == 0
